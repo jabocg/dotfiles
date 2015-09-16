e479ca85732c50ce0bebf6e7a8245d268b8001cf
Jacob Gersztyn <jgerszty@lxdebian>
2015-09-16 15:06:12 -0700

    Add install scripts

---
+++ installs/git-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git config --global user.name "Jacob Gersztyn"
+git config --global user.email "jabocg@gmail.com"
+git config --global credential.belper store
---
+++ installs/programs-rpm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# just getting some programs that I use often
+
+yum install -Y tmux termite xclip
