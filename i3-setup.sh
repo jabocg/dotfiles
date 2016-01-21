fdc2bfd8d24115c6f73743f9ec646d9fd6ac8afb
Jacob Gersztyn <jabocg@gmail.com>
2016-01-21 10:38:16 -0700

    Update i3 install script

--- i3-setup.sh
+++ i3-setup.sh
@@ -1,10 +1,10 @@
 # i3 setup 
 
 # install i3 and i3status
-dnf install i3 i3status
+sudo dnf install i3 i3status
 
 # symlink the configs and shit
-if [! -e ~/.config/i3/config ] ; then
+if [ ! -e ~/.config/i3/config ] ; then
     echo "backing up i3 config"
     mv ~/.config/i3/config ~/.config/i3/config.bak
 fi
