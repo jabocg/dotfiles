b126cead61493b987389b049e6051655d427ea98
jabocg <jabocg@gmail.com>
2016-01-20 22:34:49 -0700

    Rename setup script, create i3 setup file

---
+++ i3-setup.sh
@@ -0,0 +1,18 @@
+# i3 setup 
+
+# install i3 and i3status
+dnf install i3 i3status
+
+# symlink the configs and shit
+if [! -e ~/.config/i3/config ] ; then
+    echo "backing up i3 config"
+    mv ~/.config/i3/config ~/.config/i3/config.bak
+fi
+
+# create dir if needed
+if [ ! -e ~/.config/i3/ ] ;then
+    mkdir -p ~/.config/i3
+fi
+
+# crate symlink
+ln -s ~/git/dotfiles/i3/config ~/.config/i3/config
