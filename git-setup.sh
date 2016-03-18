b75614f8c9d9beb0d66d9811c6b9a2d25fb3fe3e
Jacob Gersztyn <jabocg@gmail.com>
2016-03-17 17:36:44 -0700

    Add editor to git config

--- git-setup.sh
+++ git-setup.sh
@@ -3,3 +3,9 @@
 git config --global user.name "Jacob Gersztyn"
 git config --global user.email "jabocg@gmail.com"
 git config --global push.default simple
+
+if [ "$(uname)" == "Darwin" ]; then # Mac OS
+    git config --global core.editor "mvim -v"
+else 
+    git config --global core.editor vim
+fi
