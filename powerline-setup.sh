bfc4ae64ff0c935bbaaf9b7073a6bd5607cbb11b
Jacob Gersztyn <jabocg@gmail.com>
2016-03-17 17:37:20 -0700

    Revamp, should work on most OSs now

--- powerline-setup.sh
+++ powerline-setup.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+# install automake and autoconf
+brew install automake
+dnf install automake
+brew install autoconf
+dnf install autoconf
 # install socat
 dnf install socat
+brew install socat
 # install psutil, python
 pip install psutil
 # req for pyuv, lib-toolize
 dnf install libtool
+brew install libtool
 # install pyuv, python
 pip install pyuv
 # install i3-py, python
@@ -13,11 +21,6 @@ pip install i3-py
 pip install powerline-status
 
 # install fonts
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-
-# Linux only stuff
-echo "use these on linux, if can't patch"
-echo "mv PowerlineSymbols.otf ~/.fonts/"
-echo "fc-cache -vf ~/.fonts/"
-echo "mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/"
+git clone git@home.github.com:powerline/fonts ~/git/powerline-fonts
+. ~/git/powerline-fonts/install.sh
+echo "install the fonts from ~/git/powerline-fonts"
