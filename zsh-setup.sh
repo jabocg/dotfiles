f0f7406c83a575fdf33e247edaa7378003322ae6
Jacob Gersztyn <jabocg@gmail.com>
2016-04-08 16:28:28 -0700

    Move zshrc link after oh-my-zsh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -4,6 +4,9 @@
 # install zsh
 dnf install zsh 
 
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # backup current zshrc
 if [ -e ~/.zshrc ] ; then 
     mv ~/.zshrc ~/.zshrc.bak
@@ -11,6 +14,3 @@ fi
 
 # create symlink
 ln -s ~/git/dotfiles/zsh/.zshrc ~/.zshrc
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
