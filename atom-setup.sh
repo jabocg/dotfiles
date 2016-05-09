dcbbe9f396b294eb5bd59e430ccf9573a7c634f6
Jacob Gersztyn <jabocg@gmail.com>
2016-05-09 09:59:46 -0700

    Add atom setup
    Mostly(actually, only) installing packages as of now. Not sure how to
    handle settings and the like.

---
+++ atom-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# setup atom stuffs(mostly packages)
+
+# install packages
+apm install activate-power-mode minimap minimap-cursorline minimap-find-and-replace minimap-git-diff scroll-sync terminal-plus
