ded81ef704f60f15f100bc0f07c7dd458cc11c1b
Jacob Gersztyn <jabocg@gmail.com>
2016-08-24 13:04:50 -0700

    update zsh install

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -2,10 +2,7 @@
 # setup for ZSH
 
 # install zsh
-dnf install zsh 
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo dnf install zsh 
 
 # backup current zshrc
 if [ -e ~/.zshrc ] ; then 
@@ -14,3 +11,6 @@ fi
 
 # create symlink
 ln -s ~/git/dotfiles/zsh/.zshrc ~/.zshrc
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
