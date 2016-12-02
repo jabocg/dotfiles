e2e0857eb38b35ffa477f1663d46f44698e773b1
jgerszty <jgerszty@prlta208macc02.care.asu.edu>
2016-12-01 17:15:22 -0700

    Move .zshrc copying below oh my zsh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -4,13 +4,14 @@
 # install zsh
 sudo dnf install zsh -y
 
-# backup current zshrc
-if [ -e ~/.zshrc ] ; then 
-    mv ~/.zshrc ~/.zshrc.bak
-fi
-
 # create symlink
 ln -s ~/git/dotfiles/zsh/.zshrc ~/.zshrc
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# backup current zshrc
+if [ -e ~/.zshrc ] ; then 
+    mv ~/.zshrc ~/.zshrc.bak
+fi
+
