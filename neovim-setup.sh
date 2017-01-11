72534a192683b5410bfaea7b2afacf53ef4f9aa4
Jacob Gersztyn <jabocg@gmail.com>
2017-01-10 17:51:28 -0700

    Update neovim setup script

--- neovim-setup.sh
+++ neovim-setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# create config structure
+mkdir -p $HOME/.config/nvim/
+mkdir -p $HOME/.config/nvim/backup
+mkdir -p $HOME/.config/nvim/undo
+
 # backup current init.vim
-if [ -e ~/.config/nvim/init.vim ] ; then
-    mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
+if [ -e $HOME/.config/nvim/init.vim ] ; then
+    mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.bak
 fi
 
 # sym-link new init.vim
-ln -s ~/git/dotfiles/neovim/init.vim ~/.config/nvim/init.vim
+ln -s $HOME/git/dotfiles/neovim/init.vim $HOME/.config/nvim/init.vim
 
 # install vim-plug
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install plugins
