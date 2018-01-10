9e43097f16f2e52ea2b562c54dc0333b67a8ec0c
Jacob Gersztyn <jabocg@gmail.com>
2018-01-09 17:50:02 -0700

    Update fzf install to be platform agnostic

--- fzf-setup.sh
+++ fzf-setup.sh
@@ -2,7 +2,15 @@
 # install and setup FZF
 
 # install ag
-sudo dnf install -y the_silver_searcher || brew install the_silver_searcher
+if hash dnf 2>/dev/null ; then
+	sudo dnf install -h the_silver_searcher
+elif hash yum 2>/dev/null ; then
+	sudo yum install the_silver_searcher
+elif hash brew 2>/dev/null ; then
+	brew install the_silver_searcher
+elif hash pacman 2>/dev/null ; then
+	sudo pacman -S the_silver_searcher
+fi
 
 # download
 git clone https://github.com/junegunn/fzf $HOME/git/fzf
