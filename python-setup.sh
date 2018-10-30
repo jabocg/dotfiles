ef563ab8d0d52834c0993d70b978a3016d5f5aa5
Jacob Gersztyn <jabocg@gmail.com>
2018-10-30 12:07:35 -0700

    Update python install to allow apt and apt-get

--- python-setup.sh
+++ python-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ecoh "Installing python..."
 if hash dnf 2>/dev/null ; then
 	sudo dnf install -y python2 python3
 elif hash yum 2>/dev/null ; then
@@ -8,13 +9,22 @@ elif hash brew 2>/dev/null ; then
 elif hash pacman 2>/dev/null ; then
 	sudo pacman --noconfirm -S python2 python
 	sudo pacman --noconfirm -S python2-pip python-pip
+elif hash apt 2>/dev/null ; then
+	sudo apt install -y python python3
+	sudo apt install -y python-pip python3-pip
+elif hash apt-get 2>/dev/null ; then
+	sudo apt-get install -y python python3
+	sudo apt-get install -y python-pip python3-pip
 fi
 
-sudo pip install virtualenv
-sudo pip3 install virtualenv
+echo "Installing virtualenv..."
+sudo pip install setuptools virtualenv
+sudo pip3 install setuptools virtualenv
 
+echo "Setting up virtualenvs..."
 virtualenv -p python2 ~/.venv/py2/
 virtualenv -p python3 ~/.venv/py3/
 
+ecoh "Installing ipython..."
 sudo pip install ipython
 sudo pip3 install ipython
