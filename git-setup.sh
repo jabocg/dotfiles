29f1e49ab3f3a133c40c7d2743ac9e376d54dc62
Jacob Gersztyn <jabocg@gmail.com>
2019-01-30 19:17:49 -0700

    Make git's pager less aggressive

--- git-setup.sh
+++ git-setup.sh
@@ -4,7 +4,11 @@ git config --global user.name "Jacob Gersztyn"
 git config --global user.email "jabocg@gmail.com"
 git config --global push.default simple
 
+# use neovim for the editor
 git config --global core.editor nvim
+
+# tell less to not paginate if less than 1 page
+git config --global core.pager "less -F -X"
 
 if [ -e ~/.gitignore_global ] ; then
     mv ~/.gitignore_global ~/.gitignore_global.bak
